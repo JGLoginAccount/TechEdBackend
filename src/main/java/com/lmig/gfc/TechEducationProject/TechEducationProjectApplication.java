ff823ca964ef1b7d9ef8e86ea63a2c30a4ae5ea8
Ted Schulte <Ted.Schulte@LibertyMutual.com>
2018-01-08 15:00:24 -0500

    update to project

--- src/main/java/com/lmig/gfc/TechEducationProject/TechEducationProjectApplication.java
+++ src/main/java/com/lmig/gfc/TechEducationProject/TechEducationProjectApplication.java
@@ -3,7 +3,7 @@ package com.lmig.gfc.TechEducationProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class TechEducationProjectApplication {
 
 	public static void main(String[] args) {
