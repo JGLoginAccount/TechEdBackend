696077e791bfa07bc39337d4b6cbb422be7519c2
John Griswold <john.griswold@libertymutual.com>
2018-01-08 14:49:41 -0500

    intialize project

---
+++ src/main/java/com/lmig/gfc/TechEducationProject/TechEducationProjectApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.TechEducationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEducationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEducationProjectApplication.class, args);
+	}
+}
