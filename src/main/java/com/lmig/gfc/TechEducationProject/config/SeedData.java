34f7837813c60ae45fc8ae29e5c720f30960bfaf
Rob Johnson <rob.johnson@libertymutual.com>
2018-01-18 11:03:54 -0500

    expanded seed data only

--- src/main/java/com/lmig/gfc/TechEducationProject/config/SeedData.java
+++ src/main/java/com/lmig/gfc/TechEducationProject/config/SeedData.java
@@ -27,61 +27,56 @@ public class SeedData {
 			ProfileRepository mentRepo, PasswordEncoder encoder) {
 
 		User admin = user.save(new User("admin", "user", "admin", encoder.encode("password")));
-		
+
 		MentorProfile adminMent = new MentorProfile("admin", "Admin", "User", "Dover", "yes", "Next Week", 5.0, "Email",
 				"1@1.com");
-		
+
 		mentRepo.save(adminMent);
-		
-	
-		skills.save(new Skills("Angular"));
-		skills.save(new Skills("C++"));
-		skills.save(new Skills("CSS"));
-		skills.save(new Skills("HTML"));
-		skills.save(new Skills("PHP"));
-		skills.save(new Skills("Ruby"));
-		Skills skills2 = new Skills("Java");
-		Skills skills3 = new Skills("JavaScript");
-		Skills skills4 = new Skills("Python");
-		Skills skills5 = new Skills("TypeScript");
+
+		// skills.save(new Skills("Angular"));
+		// skills.save(new Skills("C++"));
+		// skills.save(new Skills("CSS"));
+		// skills.save(new Skills("HTML"));
+		// skills.save(new Skills("PHP"));
+		// skills.save(new Skills("Ruby"));
+		//
+
+		Skills skills1 = new Skills("Angular");
+		Skills skills2 = new Skills("C++");
+		Skills skills3 = new Skills("CSS");
+		Skills skills4 = new Skills("HTML");
+		Skills skills5 = new Skills("Java");
+		Skills skills6 = new Skills("JavaScript");
+		Skills skills7 = new Skills("PHP");
+		Skills skills8 = new Skills("Python");
+		Skills skills9 = new Skills("Ruby");
+		Skills skills10 = new Skills("TypeScript");
 
 		MentorProfile ment = new MentorProfile("N0026982", "Mentor", "One", "Dover", "yes", "Next Week", 5.0, "Email",
 				"1@1.com");
-		// User dataUser = new User("N0026982", "Clark", "Griswold", "Chicago",
-		// "Personal", "PM");
 
 		MentorProfile ment2 = new MentorProfile("N2222222", "Mentor", "Two", "Portsmouth", "yes", "Next Two Weeks",
 				10.0, "Phone", "222-222-2222");
-		// User dataUser2 = new User("N0211099", "Ellen", "Griswold", "Chicago",
-		// "Personal", "PM");
 
 		MentorProfile ment3 = new MentorProfile("N3333333", "Mentor", "Three", "Indianpolis", "no", "", 0.0, "Phone",
 				"333-333-3333");
 
-		// User dataUser3 = new User("N0333333", "Rusty", "Griswold", "Chicago",
-		// "Personal", "PM");
-
 		MentorProfile ment4 = new MentorProfile("N4444444", "Mentor", "Four", "Seattle", "yes", "One Month or More",
 				10.0, "Email", "4@4.com");
-		// contactRepoUser dataUser4 = new User("N0444444", "Audrey", "Griswold",
-		// "Chicago", "Personal", "PM");
 
 		MentorProfile ment5 = new MentorProfile("N5555555", "Mentor", "Five", "Fairfield", "yes", "Next Three Weeks",
 				15.0, "Skype", "use lync");
 
-		// User dataUser5 = new User("N0555555", "Eddie", "Johnson", "Coolidge",
-		// "Personal", "PM");
-
-		// dataUser.setProfile(ment);
-		// dataUser2.setProfile(ment2);
-		// dataUser3.setProfile(ment3);
-		// dataUser4.setProfile(ment4);
-		// dataUser5.setProfile(ment5);
-
+		skills.save(skills1);
 		skills.save(skills2);
 		skills.save(skills3);
 		skills.save(skills4);
 		skills.save(skills5);
+		skills.save(skills6);
+		skills.save(skills7);
+		skills.save(skills8);
+		skills.save(skills9);
+		skills.save(skills10);
 
 		List<Skills> skillsSir = new ArrayList<Skills>();
 		List<Skills> skillTwo = new ArrayList<Skills>();
@@ -90,14 +85,22 @@ public class SeedData {
 		List<Skills> skillFive = new ArrayList<Skills>();
 
 		skillsSir.add(skills2);
+		skillTwo.add(skills1);
+		skillTwo.add(skills2);
 		skillTwo.add(skills3);
-		skillTwo.add(skills4);
+		skillTwo.add(skills5);
 		skillThree.add(skills3);
+		skillThree.add(skills4);
 		skillThree.add(skills5);
-		skillFour.add(skills3);
-		skillFour.add(skills4);
-		skillFive.add(skills2);
-		skillFive.add(skills5);
+		skillThree.add(skills6);
+		skillFour.add(skills5);
+		skillFour.add(skills6);
+		skillFour.add(skills7);
+		skillFour.add(skills8);
+		skillFour.add(skills10);
+		skillFive.add(skills3);
+		skillFive.add(skills9);
+		skillFive.add(skills10);
 
 		ment.setMentorSkills(skillsSir);
 		ment2.setMentorSkills(skillTwo);
